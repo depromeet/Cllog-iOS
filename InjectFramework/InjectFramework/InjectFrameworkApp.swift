85994beb988e856e2850e55cb6cb64d5b46fb5c6
linsaeng <duwjdtmd91@gmail.com>
2025-01-24 12:13:55 +0900

    init commit

---
+++ InjectFramework/InjectFramework/InjectFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  InjectFrameworkApp.swift
+//  InjectFramework
+//
+//  Created by saeng lin on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct InjectFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InjectFramework/InjectFrameworkTests/InjectFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  InjectFrameworkTests.swift
+//  InjectFrameworkTests
+//
+//  Created by saeng lin on 1/24/25.
+//
+
+import Testing
+@testable import InjectFramework
+
+struct InjectFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
