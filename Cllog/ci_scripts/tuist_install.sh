c97087d21298ebc2a98d26e9c7935428526feb63
Lee424 <tmdghks424@naver.com>
2025-02-13 12:43:26 +0900

    tuist_install 스크립트 수정

--- Cllog/ci_scripts/tuist_install.sh
+++ Cllog/ci_scripts/tuist_install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-
-brew install tuist
-export PATH="/opt/homebrew/bin:$PATH"
-
+set -e
 cd ..
 
+curl https://mise.run | sh
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "❗️Current PATH: $PATH"
+
+echo "❗️mise version"
+mise --version
+echo "❗️mise install"
+mise install
+eval "$(mise activate bash --shims)"
+
+echo "❗️tuist install"
 tuist install
+echo "❗️tuist generate"
 tuist generate --no-open
