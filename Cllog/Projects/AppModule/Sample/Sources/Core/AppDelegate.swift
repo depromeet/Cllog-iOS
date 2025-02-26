b958571d06538c3ce58f54aefe344b0c8bda14be
이준영 <junlight94@gmail.com>
2025-02-26 22:27:46 +0900

    feat. 폰트 적용

--- Cllog/Projects/AppModule/Sample/Sources/Core/AppDelegate.swift
+++ Cllog/Projects/AppModule/Sample/Sources/Core/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import DesignKit
  
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        ClLogFont.register()
         return true
     }
 
---
+++ Cllog/Projects/CllogService/CoreKit/DesignKit/Sources/Core/Font/Fonts.swift
@@ -0,0 +1,44 @@
+//
+//  Fonts.swift
+//  DesignKit
+//
+//  Created by Junyoung on 2/26/25.
+//  Copyright © 2025 Supershy. All rights reserved.
+//
+
+import CoreText
+import Foundation
+import SwiftUICore
+
+public struct ClLogFont {
+    enum Pretendard: String {
+        case thin = "Pretendard-Thin"
+        case extraLight = "Pretendard-ExtraLight"
+        case light = "Pretendard-Light"
+        case regular = "Pretendard-Regular"
+        case medium = "Pretendard-Medium"
+        case semiBold = "Pretendard-SemiBold"
+        case bold = "Pretendard-Bold"
+        case extraBold = "Pretendard-ExtraBold"
+        case black = "Pretendard-Black"
+        
+        public static let all = [
+            thin, extraLight, light, regular, medium, semiBold, bold, extraBold, black
+        ]
+    }
+}
+
+extension ClLogFont {
+    public static func register() {
+        Pretendard.all.forEach { font in
+            guard let fontURL = fontURL(for: font) else {
+                fatalError("❌ 폰트 파일을 찾을 수 없음: \(font.rawValue)")
+            }
+            CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, nil)
+        }
+    }
+    
+    fileprivate static func fontURL(for font: Pretendard) -> URL? {
+        return Bundle.module.url(forResource: font.rawValue, withExtension: "ttf")
+    }
+}
