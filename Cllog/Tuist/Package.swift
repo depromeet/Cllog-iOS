e3fa66b2283d7953f5cb3ce30b1447d973292ffc
이준영 <junlight94@gmail.com>
2025-02-26 21:21:39 +0900

    [SPS-67] 파이어페이스 연동 (#20)

--- Cllog/Tuist/Package.swift
+++ Cllog/Tuist/Package.swift
@@ -11,6 +11,7 @@ import ProjectDescriptionHelpers
             "Alamofire": .framework,
             "Then": .framework,
             "Pulse": .framework,
+            "Firebase": .framework,
         ],
         baseSettings: Settings.settings(configurations: XCConfig.framework)
     )
@@ -23,6 +24,7 @@ let package = Package(
         .package(url: "https://github.com/SnapKit/SnapKit.git", exact: "5.6.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.7.1"),
         .package(url: "https://github.com/devxoul/Then.git", exact: "3.0.0"),
-        .package(url: "https://github.com/kean/Pulse.git", exact: "5.1.2")
+        .package(url: "https://github.com/kean/Pulse.git", exact: "5.1.2"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.8.1")
     ]
 )
