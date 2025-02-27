3e22d00358aa9ebf104aa53c0ac37dcbe19ac57c
이준영 <junlight94@gmail.com>
2025-02-27 17:25:12 +0900

    [SPS-72] 디자인 시스템 폰트, 컬러 작업 (#28)

---
+++ Cllog/Projects/CllogService/CoreKit/DesignKit/Sources/Core/DesignSystem/Color.swift
@@ -0,0 +1,65 @@
+//
+//  Color.swift
+//  ClLogUI
+//
+//  Created by saeng lin on 2/19/25.
+//  Copyright © 2025 Supershy. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+extension ClLogUI where Base == UIColor {
+    
+    public static var white: UIColor { asset(#function) }
+    public static var gray10: UIColor { asset(#function) }
+    public static var gray50: UIColor { asset(#function) }
+    public static var gray100: UIColor { asset(#function) }
+    public static var gray200: UIColor { asset(#function) }
+    public static var gray300: UIColor { asset(#function) }
+    public static var gray400: UIColor { asset(#function) }
+    public static var gray500: UIColor { asset(#function) }
+    public static var gray600: UIColor { asset(#function) }
+    public static var gray700: UIColor { asset(#function) }
+    public static var gray800: UIColor { asset(#function) }
+    public static var gray900: UIColor { asset(#function) }
+    public static var primary: UIColor { asset(#function) }
+    public static var dim: UIColor { asset(#function) }
+    
+}
+
+extension ClLogUI where Base == Color {
+     
+    public static var white: Color { asset(#function) }
+    public static var gray10: Color { asset(#function) }
+    public static var gray50: Color { asset(#function) }
+    public static var gray100: Color { asset(#function) }
+    public static var gray200: Color { asset(#function) }
+    public static var gray300: Color { asset(#function) }
+    public static var gray400: Color { asset(#function) }
+    public static var gray500: Color { asset(#function) }
+    public static var gray600: Color { asset(#function) }
+    public static var gray700: Color { asset(#function) }
+    public static var gray800: Color { asset(#function) }
+    public static var gray900: Color { asset(#function) }
+    public static var primary: Color { asset(#function) }
+    public static var dim: Color { asset(#function) }
+}
+
+extension ClLogUI where Base == UIColor {
+    
+    private static func asset(_ name: String) -> UIColor {
+        let colorName = "clLogUI_" + name
+        guard let color = UIColor(named: colorName, in: .module, compatibleWith: nil) else {
+            assertionFailure("can't find color asset: \(colorName)")
+            return UIColor.clear
+        }
+        return color
+    }
+}
+
+extension ClLogUI where Base == Color {
+    private static func asset(_ name: String) -> Color {
+        return Color(uiColor: UIColor.clLogUI.asset(name) )
+    }
+}
---
+++ Cllog/Projects/CllogService/CoreKit/DesignKit/Sources/Core/DesignSystem/Fonts.swift
@@ -0,0 +1,41 @@
+//
+//  Fonts.swift
+//  DesignKit
+//
+//  Created by Junyoung on 2/26/25.
+//  Copyright © 2025 Supershy. All rights reserved.
+//
+
+import CoreText
+import Foundation
+import SwiftUICore
+
+public struct ClLogFont {
+    enum Pretendard: String, CaseIterable {
+        case thin = "Pretendard-Thin"
+        case extraLight = "Pretendard-ExtraLight"
+        case light = "Pretendard-Light"
+        case regular = "Pretendard-Regular"
+        case medium = "Pretendard-Medium"
+        case semiBold = "Pretendard-SemiBold"
+        case bold = "Pretendard-Bold"
+        case extraBold = "Pretendard-ExtraBold"
+        case black = "Pretendard-Black"
+    }
+}
+
+extension ClLogFont {
+    /// 폰트 등록
+    public static func register() {
+        Pretendard.allCases.forEach { font in
+            guard let fontURL = fontURL(for: font) else {
+                fatalError("❌ 폰트 파일을 찾을 수 없음: \(font.rawValue)")
+            }
+            CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, nil)
+        }
+    }
+    
+    fileprivate static func fontURL(for font: Pretendard) -> URL? {
+        return Bundle.module.url(forResource: font.rawValue, withExtension: "ttf")
+    }
+}
