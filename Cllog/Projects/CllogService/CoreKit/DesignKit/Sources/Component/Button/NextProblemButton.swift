3162d4a8ce13a7687cc15c8ab159bd7b35de42a5
이준영 <junlight94@gmail.com>
2025-03-01 17:08:52 +0900

    feat. 다음 문제 버튼

---
+++ Cllog/Projects/CllogService/CoreKit/DesignKit/Sources/Component/Button/NextProblemButton.swift
@@ -0,0 +1,41 @@
+//
+//  NextProblemButton.swift
+//  DesignKit
+//
+//  Created by Junyoung on 3/1/25.
+//  Copyright © 2025 Supershy. All rights reserved.
+//
+
+import SwiftUI
+
+public struct NextProblemButton: View {
+    private let onTapped: () -> Void
+    
+    public init(
+        onTapped: @escaping () -> Void
+    ) {
+        self.onTapped = onTapped
+    }
+    
+    public var body: some View {
+        Button {
+            onTapped()
+        } label: {
+            Text("다음\n문제")
+                .font(.c1)
+                .foregroundStyle(Color.clLogUI.gray10)
+                .padding(.vertical, 12)
+                .padding(.horizontal, 19.5)
+                .background(
+                    Circle()
+                        .fill(Color.clLogUI.gray600)
+                )
+        }
+    }
+}
+
+#Preview {
+    NextProblemButton() {
+        
+    }
+}
