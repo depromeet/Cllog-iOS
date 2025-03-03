5aedec549e51a50a5d4ac4166502e835621dbe1c
soi <zest1923@gmail.com>
2025-03-03 23:35:40 +0900

    refactor: 리뷰 반영

--- Cllog/Projects/CllogService/Data/Data/Sources/Storage/AppData.swift
+++ Cllog/Projects/CllogService/Data/Data/Sources/Storage/AppData.swift
@@ -9,11 +9,30 @@
 import Foundation
 
 enum AppData {
+    
+    
+    /*
+    /// 로그아웃 시 삭제할 데이터
+    enum LoginUserUserDefaultKey: String, CaseIterable {
+        
+    }
+    */
+    
+    /// 로그아웃을 해도, 삭제하지 않을 데이터
     enum UserDefaultKey: String, CaseIterable {
         case didCompleteOnboarding
+        
+        static var deletable: [UserDefaultKey] {
+            return [.didCompleteOnboarding]
+        }
+        
+        static var nonDeletable: [UserDefaultKey] {
+            return []
+        }
     }
     
-    enum KeychainKey: String, CaseIterable {
+    /// 로그아웃 시 삭제할 데이터
+    enum LoginUserKeychainKey: String, CaseIterable {
         case token
         
         var itemClass: KeychainItemType {
@@ -23,15 +42,29 @@ enum AppData {
         }
     }
     
-    @Keychain(key: KeychainKey.token.rawValue, itemClass: KeychainKey.token.itemClass)
+    /*
+    /// 로그아웃을 해도, 삭제하지 않을 데이터
+    enum KeychainKey: String, CaseIterable {
+        
+    }
+    */
+    
+    @Keychain(
+        key: LoginUserKeychainKey.token.rawValue,
+        itemClass: LoginUserKeychainKey.token.itemClass
+    )
+    
     static var token: AuthTokenDTO?
     
     static func clearLocalData() {
-        UserDefaultKey.allCases.forEach {
+        // TODO
+        /*
+        LoginUserUserDefaultKey.allCases.forEach {
             UserDefaults.standard.removeObject(forKey: $0.rawValue)
         }
+         */
         
-        KeychainKey.allCases.forEach {
+        LoginUserKeychainKey.allCases.forEach {
             try? KeychainManager.shared.deleteItem(ofClass: $0.itemClass, key: $0.rawValue)
         }
     }
