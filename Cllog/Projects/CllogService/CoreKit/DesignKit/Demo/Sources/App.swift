a601e57903b7f41c7645153ca5693cba87fdc251
이준영 <junlight94@gmail.com>
2025-03-12 22:58:41 +0900

    chore.

--- Cllog/Projects/CllogService/CoreKit/DesignKit/Demo/Sources/App.swift
+++ Cllog/Projects/CllogService/CoreKit/DesignKit/Demo/Sources/App.swift
@@ -8,30 +8,10 @@ import Shared
 struct CalendarDemoApp: App {
     
     init() {
-        ClLogDI.container.register(FutureMonthCheckerUseCase.self) { _ in
-            FutureMonthChecker()
-        }
     }
     
     var body: some Scene {
         WindowGroup {
-            CalendarMainView(
-                store: .init(
-                    initialState: CalendarMainFeature.State(),
-                    reducer: {
-                        CalendarMainFeature()
-                    }
-                )
-            )
-            
-//            CalendarDetailView(
-//                store: .init(
-//                    initialState: CalendarDetailFeature.State(),
-//                    reducer: {
-//                        CalendarDetailFeature()
-//                    }
-//                )
-//            )
         }
     }
 }
