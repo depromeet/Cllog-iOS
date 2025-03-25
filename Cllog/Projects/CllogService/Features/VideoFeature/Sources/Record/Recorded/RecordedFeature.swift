db564258f0b78f6e12d3e3708bfe824ca9681a32
이준영 <junlight94@gmail.com>
2025-03-25 23:07:36 +0900

    feat. 화면 종료 (#106)

--- Cllog/Projects/CllogService/Features/VideoFeature/Sources/Record/Recorded/RecordedFeature.swift
+++ Cllog/Projects/CllogService/Features/VideoFeature/Sources/Record/Recorded/RecordedFeature.swift
@@ -117,7 +117,7 @@ public struct RecordedFeature {
     
     public var body: some ReducerOf<Self> {
         BindingReducer()
-        Reduce(recorededCore)
+        Reduce(recordedCore)
             .ifLet(\.alert, action: \.alert)
             .ifLet(\.cragAlert, action: \.cragAlert)
     }
@@ -126,7 +126,7 @@ public struct RecordedFeature {
 
 extension RecordedFeature {
     
-    private func recorededCore(
+    private func recordedCore(
         _ state: inout State,
         _ action: Action
     ) -> Effect<Action> {
@@ -239,9 +239,6 @@ extension RecordedFeature {
                 .send(.pause)
             )
             
-        case .moveEditRecord(let path):
-            // 편집 화면 이동
-            return .none
             
         case .close:
             // 화면 닫힘
@@ -284,7 +281,7 @@ extension RecordedFeature {
         switch action {
         case .alert(.presented(.confirm)):
             // close 다이얼로그  저장안함
-            return .none
+            return .send(.close)
             
         case .alert(.presented(.cancel)):
             // close 다이얼로그  계속 편집
@@ -295,11 +292,13 @@ extension RecordedFeature {
             return .none
             
         case .cragAlert(.presented(.confirm)):
+            state.showSelectCragDifficultyBottomSheet = true
             // 암장정보 건너뛰기 alert - 저장안함
             return .none
             
         case .cragAlert(.presented(.cancel)):
             // 암장정보 건너뛰기 alert - 계속 편집
+            state.showSelectCragBottomSheet = true
             return .none
             
         case .cragAlert(.dismiss):
@@ -333,6 +332,7 @@ extension RecordedFeature {
             
         case .cragNameSkipButtonTapped:
             // 암장 선택 바텀시트 - 스킵 버튼 클릭
+            state.showSelectCragBottomSheet = false
             state.cragAlert = AlertState {
                 TextState("암장 정보 입력 건너뛰기")
             } actions: {
@@ -409,251 +409,3 @@ extension UIImage {
         return currentData.count <= targetSizeInBytes ? currentData : nil
     }
 }
-//
-//class NetworkManager {
-//    static let shared = NetworkManager()
-//    private init() {}
-//    
-//    /// 이미지를 multipart/form-data 방식으로 업로드하는 메서드
-//    /// - Parameters:
-//    ///   - url: 업로드할 서버 URL
-//    ///   - image: 업로드할 UIImage 객체
-//    ///   - fileName: 서버에 전송할 파일 이름 (예: "example.png")
-//    ///   - parameters: 추가로 전송할 텍스트 파라미터 (옵션)
-//    ///   - completion: 업로드 완료 후 반환되는 결과 클로저
-//    func uploadImage(url: URL,
-//                     image: UIImage,
-//                     fileName: String = "example.png",
-//                     parameters: [String: String] = [:],
-//                     completion: @escaping (Result<Data, Error>) -> Void) {
-//        
-//        // 필요하다면 추가 헤더 (여기서는 accept 헤더만 추가)
-//        let headers: HTTPHeaders = [
-//            "Accept": "*/*",
-//            "Authorization": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNSIsInVzZXJJZCI6MTUsImxvZ2luSWQiOiJ0ZXN0MTIzIiwicHJvdmlkZXIiOiJMT0NBTCIsImV4cCI6MTc0MjM4NDAwOX0.2MNvThmheMjEPpbUYs0hkLBt5rGIwWkTL6l04Fbuoikccw_zeg80zGUgRqkZ_N1xnQ6c3NITtIungyL2z-xBcw"
-//        ]
-//        
-//        // Alamofire의 upload 메서드 사용
-//        AF.upload(multipartFormData: { multipartFormData in
-//            // 1. 이미지 데이터를 file 필드로 추가 (서버가 file= 으로 받길 기대)
-//            if let imageData = image.pngData() {
-//                multipartFormData.append(imageData,
-//                                         withName: "file",
-//                                         fileName: fileName,
-//                                         mimeType: "image/png")
-//            }
-//            
-//            // 2. 추가 텍스트 파라미터가 있다면 함께 추가
-//            for (key, value) in parameters {
-//                if let valueData = value.data(using: .utf8) {
-//                    multipartFormData.append(valueData, withName: key)
-//                }
-//            }
-//        },
-//                  to: url,
-//                  method: .post,
-//                  headers: headers)
-//        .validate()  // HTTP 상태 코드 200~299 확인
-//        .responseData { response in
-//            switch response.result {
-//            case .success(let data):
-//                completion(.success(data))
-//            case .failure(let error):
-//                completion(.failure(error))
-//            }
-//        }
-//    }
-//}
-//public struct VideoUploadDTOss: Codable {
-//    let fileUrl: String
-//}
-//
-//struct BaseResponseDTOs<T: Decodable>: Decodable {
-////    let success: Bool
-//    let data: T?
-////    let error: ErrorResponseDTO?
-//}
-//
-//
-//extension RecordedFeature {
-//    
-//    private func reduceCore(
-//        _ state: inout State,
-//        _ action: Action
-//    ) -> Effect<Action> {
-//        switch action {
-//        case .onAppear:
-//            // 화면 진입시 호출 됌
-//            state.viewModel.play()
-//            return .merge(
-//                // 타이머 시작
-//                .send(.startListeningPlayTime),
-//                // 영상 end 이벤트
-//                .send(.startListeningEndPlay)
-//            )
-//            
-//        case .startListeningEndPlay:
-//            return .run { [weak viewModel = state.viewModel] send in
-//                guard let viewModel else { return }
-//                for await _ in viewModel.playEndAsyncStream {
-//                    _ = await viewModel.seek(.zero)
-//                    viewModel.play()
-//                }
-//            }.cancellable(id: "EndPlay", cancelInFlight: true)
-//            
-//        case .startListeningPlayTime:
-//            return .run { [weak viewModel = state.viewModel] send in
-//                guard let viewModel else { return }
-//                for await (playTime, totalduration) in viewModel.playTimeAsyncStream {
-//                    await send(.timerTicked(playTime, totalduration))
-//                }
-//            }.cancellable(id: "PlayTime", cancelInFlight: true)
-//            
-//        case .timerTicked(let playTime, let duration):
-//            let totalTime = CGFloat(CMTimeGetSeconds(duration))
-//            let currentTime = CGFloat(CMTimeGetSeconds(playTime))
-//            state.duration = playTime.formatTimeInterval()
-//            state.progress = currentTime/totalTime
-//            return .none
-//            
-//        case .editButtonTapped:
-//            return .merge(
-//                .send(.moveEditRecord(state.path)),
-//                .send(.pause)
-//            )
-//            
-//        case .pause:
-//            state.viewModel.pause()
-//            return .none
-//            
-//        case .moveEditRecord:
-//            return .none
-//            
-//        case .closeButtonTapped:
-//            state.alert = AlertState {
-//                TextState("기록 저장 취소")
-//            } actions: {
-//                ButtonState(action: .cancel) {
-//                    TextState("계속 편집")
-//                }
-//                ButtonState(action: .confirm) {
-//                    TextState("저장 안함")
-//                }
-//            } message: {
-//                TextState("이 페이지를 나가면 촬영하신 영상이 저장되지 않아요")
-//            }
-//            return .run { send in
-//                await send(.pause)
-//            }
-//            
-//        case .close:
-//            return .none
-//            
-//        case .successTapped:
-//            state.climbingResult = .success
-//            state.showSelectCragBottomSheet = true
-//            return .run { send in
-//                await send(.pause)
-//            }
-////            return .merge(
-////                .send(.pause),
-////                .run(operation: { [state] send in
-////                    do {
-////                        let generator = AVAssetImageGenerator(asset: AVAsset(url: state.path))
-////                        generator.appliesPreferredTrackTransform = true
-////                        let cmTime = CMTime(seconds: 0, preferredTimescale: 600)
-////                        let thumbnail = try await generator.image(at: cmTime)
-////                        await send(.upload(type: .success, image: UIImage(cgImage: thumbnail.image)))
-////
-////                    } catch {
-////                        print("에러")
-////                    }
-////                })
-////            )
-//            
-//        case .upload(let type, let image):
-//            return .run { send in
-//                do {
-//                    let videothumbnails = try await videoUsecase.execute(name: "MyImages", fileName: "MyImages.png", mimeType: "image/png", value: image.resizedPNGData()!)
-//                    print("videothumbnails : \(videothumbnails)")
-//                } catch {
-//                    print("videothumbnails error")
-//                }
-//            }
-//        case .failtureTapped:
-//            state.climbingResult = .success
-//            state.showSelectCragBottomSheet = true
-//            return .run { send in
-//                await send(.pause)
-//            }
-//            
-//        case .alert(.presented(.confirm)):
-//            return .none
-//            
-//        case .alert(.presented(.cancel)):
-//            return .none
-//            
-//        case .alert(.dismiss):
-//            state.viewModel.play()
-//            return .none
-//            
-//        case .cragAlert(let action):
-//            switch action {
-//            case .dismiss:
-//                return .none
-//            case .presented(let dilogAction):
-//                switch dilogAction {
-//                case .cancel:
-//                    return .none
-//                case .confirm:
-//                    return .none
-//                }
-//            }
-//            
-//        case .binding(_):
-//            return .none
-//            
-//        case .cragBottomSheetAction(let isPresent):
-//            // 화면 닫고
-//            state.showSelectCragBottomSheet = isPresent
-//            
-//            return .run { send in
-//                await send(.onAppear)
-//            }
-//            
-//        case .cragNameSkipButtonTapped:
-//            // 암장을 건너뛰겠습니까?
-//            state.cragAlert = AlertState {
-//                TextState("암장 정보 입력 건너뛰기")
-//            } actions: {
-//                ButtonState(action: .cancel) {
-//                    TextState("계속 편집")
-//                }
-//                ButtonState(action: .confirm) {
-//                    TextState("저장 안함")
-//                }
-//            } message: {
-//                TextState("임장 입력을 하지 않으면 기록은 가능하지만,\n암장 관련 정보는 저장되지 않아요")
-//            }
-//            
-//            return .none
-//            
-//        case .cragName(let keyWord):
-//            return .none
-//            
-//        case .cragSaveButtonTapped(let designCrag):
-//            // 클라이밍 리스트 요청
-//            state.showSelectCragBottomSheet = false
-//            state.selectedDesignCrag = designCrag
-//            return .run { [designCrag] send in
-//                await send(.cragBottomSaveButtonTapped(designCrag))
-//            }
-//            
-//        case .cragBottomSaveButtonTapped(let designCrag):
-//            state.showSelectCragDifficultyBottomSheet = true
-//            return .none
-//        case .play:
-//            return .none
-//        }
-//    }
-//}
